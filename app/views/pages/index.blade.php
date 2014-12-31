b29c869adebe6876675484f27ddd5b463ecd0f1e
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-01 03:47:22 +0530

    added some more sections

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -7,9 +7,49 @@ Elevator (by Horntell)
 @section('content')
 
 	<section class="head-banner">
-		<h2 class="font-bold">Hours of startup wisdom from influencers</h1>
-		<h3 class="font-thin">in under 5-minutes</h2>
-		<a href target="_blank" class="btn btn-cta font-thin">Start <i class="fa fa-angle-double-right"></i></a>
+		<h2 class="font-bold">Hours of startup wisdom from influencers</h2>
+		<h3 class="font-thin">in under 5-minutes</h3>
+		<a href target="_blank" class="btn btn-cta font-thin">See a sample <i class="fa fa-angle-double-right"></i></a>
 	</section>
 
+	<section class="secondary-banner">
+		<h3 class="font-thin">A tiny email filled with knowledge, delivered thrice a week.</h3>
+	</section>
+
+	<section class="container">
+		<div class="row segment-box">
+			<div class="col-md-6 col-md-offset-6">
+				<h4 class="font-bold">Across the Twitter</h4>
+				<p>I’ll hunt the 5 thought-provoking tweets to get your day started.</p>
+			</div>
+		</div>
+		<div class="row segment-box">
+			<div class="col-md-6 col-md-offset-6">
+				<h4 class="font-bold">Across the blogs</h4>
+				<p>I’ll find and summarize the awesome blog posts in under 140 words for you to skim through.</p>
+			</div>
+		</div>
+		<div class="row segment-box">
+			<div class="col-md-6 col-md-offset-6">
+				<h4 class="font-bold">Across the Youtube</h4>
+				<p>I’ll collect, transcribe and summarize the interesting videos.</p>
+			</div>
+		</div>
+		<div class="row segment-box">
+			<div class="col-md-6 col-md-offset-6">
+				<h4 class="font-bold">Useful products</h4>
+				<p>I’ll send you my most loved 3 hunts from ProductHunt for you.</p>
+			</div>
+		</div>
+		<div class="row segment-box">
+			<div class="col-md-6 col-md-offset-6">
+				<h4 class="font-bold">One Last Thing</h4>
+				<p>And then one more unique and interesting thing with each email.</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="me">
+		<h3 class="font-thin">Hi, I'm Mohit!</h3>
+	</section>
 @endsection
