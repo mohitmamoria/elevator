c5ead9286bbdc34a25f613b5c62df0abe337c7ab
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-04 02:35:23 +0530

    finished the sample edition

--- app/views/layouts/edition.blade.php
+++ app/views/layouts/edition.blade.php
@@ -5,104 +5,22 @@ Elevator (by Horntell)
 @endsection
 
 @section('content')
-<section class="bg-brand-primary">
-	<a href="/"><img src="/assets/images/elevator-small.png" alt="Elevator" class="img-responsive img-center"></a>
-</section>
+	<section class="bg-brand-primary">
+		<a href="/"><img src="/assets/images/elevator-small.png" alt="Elevator" class="img-responsive img-center"></a>
+	</section>
 
 <div class="container">
-	<div class="col-md-10 col-md-offset-1">
+	<div class="col-md-8 col-md-offset-2">
 		<div class="title-area">
-			<h1 class="font-thin">Stop Talking, Start Doing!</h1>
-			<p><em>Jan 02, 2015</em></p>
+			<h1 class="font-thin">{{ $edition['title'] or 'Stop Talking, Start Doing!' }}</h1>
+			<p><em>{{ $edition['created_at'] or 'Jan 02, 2015'}}</em></p>
 		</div>
-		<section class="panel no-borders">
-			<div class="panel-body">
-				<div class="row">
-					<div class="col-md-12">
-						<p>Winters - the perfect season to laze around! But do you know, there are two guys at some far-off place, sitting in a basement, wrapped in blankets and <em>doing</em> the thing that you're only <em>planning</em> to do tomorrow.</p>
-						<p>I don't know who they are. It's very probable that they are in their early 20s but I am not sure how they look like. I don't even know what they are working on, but I know one thing for sure - they are hustling right now (and it's past midnight in their timezone).</p>
-						<p>They might fail at whatever they are doing. But does it matter? Once fallen, they will get up, dust their shirts off and start afresh. And someday you will find them all over the internet as yet another <strong>overnight success story</strong>.</p>
-						<img src="/assets/images/overnight-success.png" alt="Overnight success takes years." class="img-center img-responsive img-thumbnail">
-						<p class="text-center text-xs"><a href="http://startupquote.com/post/1645456526">http://startupquote.com/post/1645456526</a></p>
-					</div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="col-md-12">
-						<h2>Hustle</h2>
-						<p>Who puts it better than everyone else? <strong>Gary Vaynerchuk</strong>. If you want to do one favour to yourself today, then follow this guy everywhere you can - <a href="http://twitter.com/mohitmamoria">Twitter</a>, <a href="https://www.youtube.com/user/GaryVaynerchuk">Youtube</a> and <a href="https://www.facebook.com/gary">Facebook</a>.</p>
-						<blockquote>
-							<p>The dream is free, but the hustle is sold separately.</p>
-							<small>Gary Vaynerchuk</small>
-						</blockquote>
-
-						<p>Inspired by Gary's style, I too once tried writing a blog post about hustle. Here's the whole post in under 140-words.</p>
-
-						<section class="panel hbox">
-							<aside class="bg-info r-l text-center v-middle">
-								<div class="wrapper">
-									<h2 class="font-thin"><em>Want weekends off? Work your ass off, first!</em></h2>
-								</div>
-							</aside>
-							<aside>
-								<div class="pos-rlt">
-									<span class="arrow left hidden-xs"></span>
-									<div class="panel-body font-thin">
-										<p>The post starts with a simple question - <strong>"What price are you ready to pay for your dreams?"</strong> And keeps the momentum up throughout by pressing the point that "things that are worth becoming your dream, should not and do not come for free". And the price for your dreams is the "work". You gotta work. Every. Single. Day.</p>
-
-										<blockquote>No one's busy. Stop watching fucking 'Games of Thrones'.</blockquote>
-
-										<p>And the simple answer to your "I-will-burnout-quickly" argument is to not go to bed satisfied. Anxiety when going to bed will help you kickstart your next day.</p>
-
-										<p>And in closing, there's an action plan - take out a piece of paper, write down your dream, price you'd pay and the deadline. And read the paper everyday, twice.</p>
-									</div>
-									<footer class="panel-footer">
-										<a href="http://mohitmamoria.com/work-your-ass-off" class="text-sm">Read Complete Post <i class="fa fa-angle-double-right"></i></a>
-									</footer>
-								</div>
-							</aside>
-						</section>
-					</div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="col-md-12">
-						<h2>Meaningful Hustle</h2>
-						<p>Now, I've learnt that we should not just hustle, but hustle for a purpose. Hustling at a day-job you hate is a waste. Instead, do the thing you love from 9pm to 2am and do it with all your heart.</p>
-						<blockquote>
-							<p>Do what you love as hard as you can.</p>
-							<small>Gary Vaynerchuk (I am telling you, <a href="http://twitter.com/garyvee">follow this guy</a>!)</small>
-						</blockquote>
-						<p>I was reading a book (The One Thing by Gary Keller) and the first page had the best thing I've read in recent times.</p>
-						<img src="/assets/images/postage-stamp.jpg" alt="Be like a postage stamp." class="img-center img-responsive img-thumbnail">
-					</div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="col-md-12">
-						<p>And in the closing, I leave you with something I found across the Twitter to shake you up.</p>
-						<blockquote class="twitter-tweet tw-align-center" lang="en"><p>People who say it cannot be done should not interrupt those who are doing it. -George Bernard Shaw</p>&mdash; SOPHIA AMORUSO (@Sophia_Amoruso) <a href="https://twitter.com/Sophia_Amoruso/status/549612161795129344">December 29, 2014</a></blockquote>
-
-						<blockquote class="twitter-tweet tw-align-center" lang="en"><p>This explains the problem with Thinkers &amp; Talkers. Things don&#39;t move unless you do it! <a href="http://t.co/j8AFPK3SR1">pic.twitter.com/j8AFPK3SR1</a></p>&mdash; PJ (@BeingPractical) <a href="https://twitter.com/BeingPractical/status/538919198634676224">November 30, 2014</a></blockquote>
-
-						<blockquote class="twitter-tweet tw-align-center" lang="en"><p>Don&#39;t be afraid to start over. It&#39;s a brand new opportunity to rebuild what you truly want.</p>&mdash; banksy (@thereaIbanksy) <a href="https://twitter.com/thereaIbanksy/status/550739429636460544">January 1, 2015</a></blockquote>
-
-						<blockquote class="twitter-tweet tw-align-center" lang="en"><p>Make zero resolutions. Just do shit. Do. All. The shit.</p>&mdash; Rae Hoffman (@sugarrae) <a href="https://twitter.com/sugarrae/status/550463569758670848">January 1, 2015</a></blockquote>
-						<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-					</div>
-				</div>
-				<hr />
-				<div class="row">
-					<div class="col-md-12">
-						<p>Remember, those two guys in the basement are actually doing their shit. Every fucking day. And you do not need January 1st to change your life.</p>
-					</div>
-				</div>
-			</div>
-		</section>
+		
+		@yield('edition')
 	</div>
 </div>
 <section class="bg-brand-primary text-center wrapper">
-	<h2 class="font-bold">How about receiving this in your inbox?</h2>
+	<h2 class="font-bold">How about receiving these in your inbox?</h2>
 	<h3 class="font-thin">twice a week</h3>
 	<p><em>(nothing dull ever, promise)</em></p>
 	<div class="form-group container">
@@ -116,9 +34,13 @@ Elevator (by Horntell)
 					<label class="pull-left">Email</label>
 					<input type="email" class="form-control" placeholder="Email">
 				</div>
-				<button type="submit" class="btn btn-cta">Get in the elevator! :)</button>
+				<button type="submit" class="btn btn-cta">Join The Elevator! :)</button>
 			</div>
 		</form>
 	</div>
 </section>
+@endsection
+
+@section('scripts')
+	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 @endsection
