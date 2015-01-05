21d77abe953159e01c094f0c25ea3be33d3ac8c6
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-05 11:44:17 +0530

    finished the landing page

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -15,6 +15,7 @@ Elevator (by Horntell)
 
 	<section class="secondary-banner">
 		<h3 class="font-thin">A tiny email filled with knowledge, delivered twice a week.</h3>
+		<p class="font-thin">(first edition releasing on February 1, 2015)</p>
 		<div class="form-group container wrapper">
 			<form action="/join" method="POST">
 				<div class="col-md-8 col-md-offset-2">
@@ -32,11 +33,50 @@ Elevator (by Horntell)
 		</div>
 	</section>
 
-	<section class="text-center">
-		
+	<section class="container">
+		<div class="row">
+			<div class="col-sm-4 wrapper-xl text-justify">
+				<img src="/assets/images/icons/twitter.png" alt="Across the Twitter" class="img-responsive img-center">
+				<h3 class="text-center">Across the Twitter</h3>
+				<p>There's so much wisdom spread around by influencers as well as the others. I’ll hunt down handful of these to get your day started.</p>
+			</div>
+			<div class="col-sm-4 wrapper-xl text-justify">
+				<img src="/assets/images/icons/blog.png" alt="Across the Blogs" class="img-responsive img-center">
+				<h3 class="text-center">Across the Blogs</h3>
+				<p>But they are so long! I’ll find and summarize the awesome blog posts in under 140 words for you to skim through.</p>
+			</div>
+			<div class="col-sm-4 wrapper-xl text-justify">
+				<img src="/assets/images/icons/youtube.png" alt="Across the Youtube" class="img-responsive img-center">
+				<h3 class="text-center">Across the Youtube</h3>
+				<p>There's so much to consume and so less time. I’ll collect, transcribe and summarize the interesting videos for you.</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-4 wrapper-xl text-justify">
+				<img src="/assets/images/icons/producthunt.png" alt="Across the Products" class="img-responsive img-center">
+				<h3 class="text-center">Across the Products</h3>
+				<p>Need something new to try out? I will send you my favourite 3 products from ProductHunt for you to check out.</p>
+			</div>
+			<div class="col-sm-4 wrapper-xl text-justify">
+				<img src="/assets/images/icons/bookshelf.png" alt="Across the Books" class="img-responsive img-center">
+				<h3 class="text-center">Across the Books</h3>
+				<p>I read a lot of books. (A Lot!) I will send the most interesting excerpts from them to you. <em>They will shake you up!</em></p>
+			</div>
+			<div class="col-sm-4 wrapper-xl text-justify">
+				<img src="/assets/images/icons/shoeprints.png" alt="And from everywhere else" class="img-responsive img-center">
+				<h3 class="text-center">And from everywhere else</h3>
+				<p>Knowledge is not limited to just internet. Sometimes you meet someone who enlightens you. All of these will be send across to you.</p>
+			</div>
+		</div>
 	</section>
 
-	<section class="container">
+	<section class="secondary-banner">
+		<h3 class="font-thin">People say I write well. Thus a very short story included with every edition.</h3>
+		<a href="/editions/sample" class="btn btn-brand-primary font-thin">See a sample <i class="fa fa-angle-double-right"></i></a>
+	</section>
+
+	<!-- <section class="container">
 		<div class="row segment-box">
 			<div class="col-md-6 col-md-offset-6">
 				<h4 class="font-bold">Across the Twitter</h4>
@@ -67,7 +107,7 @@ Elevator (by Horntell)
 				<p>And then one more unique and interesting thing with each email.</p>
 			</div>
 		</div>
-	</section>
+	</section> -->
 
 	<section id="me" class="bg-brand-primary">
 		<div class="container">
