497be264bec955a15c993c690a00fb01a118a9e5
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-05 13:24:21 +0530

    finished the thanks page

--- app/views/_partials/footer.blade.php
+++ app/views/_partials/footer.blade.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<ul class="list-inline text-center">
 			<li>
-				<em class="font-thin">(a personal adventure of <a href="http://twitter.com/mohitmamoria">@mohitmamoria</a>)</em>
+				<em class="font-thin">(an adventure by <a href="http://twitter.com/mohitmamoria">@mohitmamoria</a>)</em>
 			</li>
 		</ul>
 	</div>
--- app/views/subscription/thanks.blade.php
+++ app/views/subscription/thanks.blade.php
@@ -13,7 +13,10 @@ Elevator (by Horntell)
 			<h1 class="font-thin">Awesome!</h1>
 			<p>You've successfully joined the Elevator.</p>
 			<hr />
-			<p>Think your pals will enjoy the ride too? Share the Elevator.</p>
+			<p>Think your pals will enjoy the lift too? Share the Elevator.</p>
+			<a href="https://twitter.com/intent/tweet?button_hashtag=JoinTheElevator&text=A%205-minute%20long%20email%20filled%20with%20startup%20knowledge%20from%20influencers%2C%20delivered%20twice%20a%20week." class="twitter-hashtag-button" data-size="large" data-related="mohitmamoria,horntell" data-url="http://elevator.horntell.com">Tweet #JoinTheElevator</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 		</div>
 	</div>
+</div>
 @endsection
