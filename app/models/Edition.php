a5520617aa514f0fc25b27d34343df31176eb7bb
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-30 11:43:21 +0530

    made name mutator

--- app/models/Edition.php
+++ app/models/Edition.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Edition extends Eloquent {
 
-	protected $fillable = ['name', 'slug', 'published_at'];
+	protected $fillable = ['name', 'slug', 'is_sample', 'published_at'];
 
 	protected $dates = ['created_at', 'updated_at', 'published_at'];
 
@@ -17,6 +17,12 @@ class Edition extends Eloquent {
 		if( ! is_null($edition)) return $edition;
 
 		throw (new ModelNotFoundException)->setModel(get_called_class());
+	}
+
+	public function setNameAttribute($name)
+	{
+		$this->attributes['name'] = $name;
+		$this->attributes['slug'] = Str::slug($name);
 	}
 
 }
