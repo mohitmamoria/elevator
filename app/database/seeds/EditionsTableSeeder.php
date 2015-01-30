b3a8d6ac091e0454365a936f8b20128ff1bb6e48
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-30 11:42:54 +0530

    made seeder to seed sample edition

---
+++ app/database/seeds/EditionsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+class EditionsTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Edition::create([
+			'name' => 'Stop Talking, Start Doing!',
+			'is_sample' => true,
+			'published_at' => '2014-01-02 00:00:00'
+		]);
+	}
+
+}
