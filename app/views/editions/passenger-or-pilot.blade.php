ef65b1a2cc0f50f763d2064e926c7b802453cfff
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-04 14:54:43 +0530

    finished the edition

---
+++ app/views/editions/passenger-or-pilot.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.edition')
+
+{{-- Passenger or Pilot --}}
+
+@section('edition')
+	<article class="text-edition">
+		<p>A few weeks back, I received a call from an aspiring entrepreneur and he asked me for some life lessons that I have learnt the hard way. I gave him two.</p>
+		<ol>
+			<li>You will always have a choice. Always.</li>
+			<li>So does everyone else.</li>
+		</ol>
+
+		<p>Among all the choices, there's an important choice that you'll have to make - <strong>"Whose choice will you follow to lead your life? Yours or theirs? Will you be a passenger or the pilot?"</strong></p>
+
+		<p>In a flight, the pilot and passengers end up at the same destination. But destination doesn't matter as much as the journey does. While a passenger gets to see only through tiny side-windows, the pilot gets to see right in the face of sky and <em class="font-bold">control the whole fucking aeroplane</em>.</p>
+	</article>
+
+	<article class="text-edition">
+		<h2 class="heading-edition">Take The Control</h2>
+		<p>From a book that I had read long back, here's one phrase that had stuck to me till date.</p>
+		<blockquote class="block">
+			<p>When we are afraid to say NO, our YES is compromised.</p>
+			<small>A book called "Boundaries"</small>
+		</blockquote>
+		<p>Talking about saying a big fucking "No" to someone, let's see what we've got today.</p>
+	</article>
+
+	<article class="summary text-edition spaced-out-h padder-v">
+		<h4 class="heading-edition">How to Say "No" to a Customer Asking for a Discount</h4>
+		<p>This will happen a lot. People will ask discounts for a lot of reasons - "<em>we are bootstrapped</em>", "<em>we are a non-profit</em>", "<em>we are a very small team</em>".</p>
+
+		<blockquote><p>People who sign up because of discounts, are less valuable to your business.</p></blockquote>
+
+		<p>You cannot <em>always</em> prevent the unpleasantness of saying "No" to your customers. But here's one approach that worked best for people at Groove.</p>
+
+		<img src="https://www.groovehq.com/attachments/learn/how-to-say-no-to-discount-requests/script.png" alt="How to say No to customers asking for discounts." class="img-center img-responsive img-thumbnail">
+
+		<p>It works because it's <strong>personal</strong>, <strong>compelling</strong> and <strong>demands an action</strong>.</p>
+
+		<a href="https://www.groovehq.com/learn/how-to-say-no-to-discount-requests" class="text-sm">Read Complete Post <i class="fa fa-angle-double-right"></i></a>
+	</article>
+
+	<article class="text-edition">
+		<h2 class="heading-edition">"Smurf it up!"</h2>
+		<p>Gary Vaynerchuk has made the phrase popular. According to Gary - <em>"Do what you love. You can monetize anything."</em></p> This 35 seconds video is what you need to watch. :)
+		<iframe width="100%" height="480" src="https://www.youtube.com/embed/kgVh2c5r5JM?rel=0" frameborder="0" allowfullscreen></iframe>
+		<p>I will strongly recommend you to subscribe to Gary Vaynerchuk's youtube channel and get the <strong>absolute raw</strong> knowledge. No bullshit.</p>
+		<script src="https://apis.google.com/js/platform.js"></script>
+		<div class="g-ytsubscribe" data-channel="GaryVaynerchuk" data-layout="full" data-count="default"></div>
+		<p><em class="text-sm">No, I am not being paid to endorse him. I endorse him because I want to. I am a pilot. ;)</em></p>
+	</article>
+
+	<article class="summary text-edition spaced-out-h padder-v">
+		<h4 class="heading-edition">Product Strategy means saying "No"</h4>
+		<p>Building a great product doesn't mean hundreds of useful feature. It is about delivering a cohesive feature-set within well defined parameters.</p>
+
+		<blockquote><p>If you’re building a product, you have to be great at saying no. Not “maybe” or “later”. The only word is <strong>no</strong>.</p></blockquote>
+
+		<p>There are always a lot reasons to say Yes.</p>
+
+		<ul>
+			<li><strong>Data says so</strong>: Add Tetris to your product and you’ll probably see a boost in engagement, but does that mean your product is better? Engagement is not everything.</li>
+			<li><strong>It'll take a few minutes</strong>: A feature is like an iceberg. Estimated cost is just the tip of an iceberg. Actual cost is under the waters.</li>
+			<li><strong>Customer will leave otherwise</strong>: How to make terrible software: 1. Make promises for one-off features for a single client. 2. Implement them. 3. Repeat.</li>
+			<li><strong>We can make it optional</strong>: It removes complexity from the outer layer, but adds to it everywhere else. Less configuration the better!</li>
+			<li><strong>Competitors already have it</strong>: That doesn’t mean it’s a good idea. It could be something they’re trying out. It could be a shit idea. It could be something they’re planning on killing.</li>
+			<li><strong>This could be one</strong>: You can speculate that any unbuilt feature could transform your product. But speculation is all it is, nothing more.</li>
+		</ul>
+
+		<p>But "no" is important because no one keeps crap ideas on their roadmap. Saying no to shit features is easy. Real product decisions mean saying, "This is a really great idea, I can see why our customers would like it. Well done. <strong>But we’re not going to build it.</strong> Instead, here’s what we’re doing."</p>
+
+		<a href="http://blog.intercom.io/product-strategy-means-saying-no" class="text-sm">Read Complete Post <i class="fa fa-angle-double-right"></i></a>
+	</article>
+
+	<article class="text-edition">
+		<h2 class="heading-edition">Tid-bits</h2>
+		<blockquote class="block">
+			<p>We either make ourselves miserable or we make ourselves strong. The amount of work is the same.</p>
+			<small>Carlos Castenada</small>
+		</blockquote>
+		
+		<img src="/assets/images/passenger-or-pilot/things-matter-most.jpg" alt="Be like a postage stamp." class="img-center img-responsive img-thumbnail">
+		<p class="text-center text-xs">"The ONE Thing" by Gary Keller</p>
+
+		<img src="/assets/images/passenger-or-pilot/start-the-conversation.jpg" alt="Be like a postage stamp." class="img-center img-responsive img-thumbnail">
+		<p class="text-center text-xs"><a href="http://movemequotes.tumblr.com/post/109970905327/one-conversation-can-change-everything-more">Source</a></p>
+	</article>
+
+	<hr />
+
+	<article class="text-edition">
+		<p>Like a flight, in life too, we're going end up at the same destination eventually - facing <strong>death</strong>. But we always remember people by the way they lived, not how they died. How would you like to be remembered - as the pilot or a mere passenger?</p>
+
+		<p>Tell the world you are a pilot! <span class="text-sm">(and we'll help spread the word)</span></p>
+		<p class="text-center"><a href="https://twitter.com/intent/tweet?button_hashtag=PilotOfLife&text=I've%20just%20made%20the%20choice%20to%20be%20the%20pilot%20of%20my%20own%20life.%20No%20more%20a%20mere%20passenger." class="twitter-hashtag-button" data-size="large" data-related="mohitmamoria,horntell" data-url="http://elevator.horntell.com/editions/passenger-or-pilot">Tweet #PilotOfLife</a></p>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</article>
+@endsection
