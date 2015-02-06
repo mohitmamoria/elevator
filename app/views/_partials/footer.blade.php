4b5b99c7f9f92839b99965f637fe1b4757571862
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-06 11:41:41 +0530

    Added horntell in the footer

--- app/views/_partials/footer.blade.php
+++ app/views/_partials/footer.blade.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<ul class="list-inline text-center">
 			<li>
-				<em class="font-thin">(an adventure by <a href="http://twitter.com/mohitmamoria">@mohitmamoria</a>)</em>
+				<em class="font-thin">(an adventure by <a href="http://twitter.com/mohitmamoria">@mohitmamoria</a>, powered by <a href="http://horntell.com">Horntell</a>)</em>
 			</li>
 		</ul>
 	</div>
