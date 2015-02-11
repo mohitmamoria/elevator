a572f795a094aa6974cc4fa911dfa027886cad75
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-11 11:21:02 +0530

    added the story in little things

---
+++ app/views/editions/the-little-things.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.edition')
+
+{{-- The Little Things --}}
+
+@section('edition')
+	<article class="text-edition">
+		<p>I am lazy. Like most geeks (and unlike most gentlemen), I trim my beard only once in a week. Reason? Because it doesn't grow so much in a day that it shows up. But interestingly, after a week, it does. Every Sunday, I think the same thing, <em>"This is so interesting. Beard grows very little in a day, but when it happens for 7 days in a row - boom - there it is, making me look nerdier."</em></p>
+
+		<p>Don't all the great things in life (and startups), happen the same way? Walking a long mile happens one step at a time. Completing a book happens one page at a time. Composing a song happens one note at a time. Building this Elevator is happening one edition at a time.</p>
+
+		<p>All these little things don't look significant individually, but together, they make big things happen. A single Lego piece is of no great value. But when you've got 65,000 of those little devils, this shit happens:</p>
+
+		<img src="/assets/images/the-little-things/lego-shuttle.jpg" alt="Lego Space Shuttle" class="img-center img-responsive img-thumbnail">
+		<p class="text-center text-xs"><a href="http://www.oddee.com/item_96645.aspx">Source</a></p>
+	</article>
+
+	<article class="text-edition wisdom wisdom-orange" id="10-percent-growth">
+		<span class="wisdom-label"><a href="#10-percent-growth"><i class="fa fa-anchor"></i></a>Tips</span>
+		<h4 class="heading-edition">"Just" 10% every week</h4>
+		<p>On the same note, Paul Graham has a very interesting and yet simple growth plan for startups.</p>
+		
+		<blockquote class="text-sm">
+			<p>If you have 100 users, you need to get 10 more next week to grow 10% a week. And while 110 may not seem much better than 100, if you keep growing at 10% a week you'll be surprised how big the numbers get. After a year you'll have 14,000 users, and after 2 years you'll have 2 million.</p>
+		</blockquote>
+	</article>
+@endsection
