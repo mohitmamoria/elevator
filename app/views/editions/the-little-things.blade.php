718027deacbfe0a88c5bde3ce0d8599f4a01e559
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-12 13:27:59 +0530

    draft one ready

--- app/views/editions/the-little-things.blade.php
+++ app/views/editions/the-little-things.blade.php
@@ -6,7 +6,7 @@
 	<article class="text-edition">
 		<p>I am lazy. Like most geeks (and unlike most gentlemen), I trim my beard only once in a week. Reason? Because it doesn't grow so much in a day that it shows up. But interestingly, after a week, it does. Every Sunday, I think the same thing, <em>"This is so interesting. Beard grows very little in a day, but when it happens for 7 days in a row - boom - there it is, making me look nerdier."</em></p>
 
-		<p>Don't all the great things in life (and startups), happen the same way? Walking a long mile happens one step at a time. Completing a book happens one page at a time. Composing a song happens one note at a time. Building this Elevator is happening one edition at a time.</p>
+		<p>Don't all the big things in life (and startups), happen the same way - starting small? Walking a long mile happens one step at a time. Completing a book happens one page at a time. Composing a song happens one note at a time. Building this Elevator is happening one edition at a time.</p>
 
 		<p>All these little things don't look significant individually, but together, they make big things happen. A single Lego piece is of no great value. But when you've got 65,000 of those little devils, this shit happens:</p>
 
@@ -17,10 +17,97 @@
 	<article class="text-edition wisdom wisdom-orange" id="10-percent-growth">
 		<span class="wisdom-label"><a href="#10-percent-growth"><i class="fa fa-anchor"></i></a>Tips</span>
 		<h4 class="heading-edition">"Just" 10% every week</h4>
-		<p>On the same note, Paul Graham has a very interesting and yet simple growth plan for startups.</p>
 		
+		<p>On the similar note, <a href="https://twitter.com/paulg">Paul Graham</a> has a very interesting and yet simple growth plan for startups.</p>
 		<blockquote class="text-sm">
 			<p>If you have 100 users, you need to get 10 more next week to grow 10% a week. And while 110 may not seem much better than 100, if you keep growing at 10% a week you'll be surprised how big the numbers get. After a year you'll have 14,000 users, and after 2 years you'll have 2 million.</p>
 		</blockquote>
+	</article>
+
+	<article class="text-edition">
+		<p>Warren Buffet believes the same. When asked how to get smarter at a conference, Warren Buffett held up a stack of papers and said:</p>
+		<blockquote class="block">
+			<p>“Read 500 pages like this every day. That’s how knowledge works. It builds up, like compound interest. All of you can do it, but I guarantee not many of you will.”</p>
+		</blockquote>
+
+		<p>Ah, interesting! So, it seems that even though we all can do the hard work, most of us don't do it because the results are far in future. But hey, "far future" is still nearer than "never". Get up and start. Start small. But start.</p>
+	</article>
+
+	<article class="text-edition wisdom wisdom-red" id="gary-corner">
+		<span class="wisdom-label"><a href="#gary-corner"><i class="fa fa-anchor"></i></a> Gary's Corner</span>
+		<h4 class="heading-edition">1 > 0</h4>
+		<p>How big is the number <strong>'1'</strong>? Not much. But, it is bigger than the number <strong>'0'</strong>. People don't start because they want to go big right from the start. And because they don't see a way to get big immediately, they keep delaying for that mythical perfect moment, which never arrives.</p>
+		<p>Gary had been on interviews carried out by individuals with a very tiny following. Because in his words: <strong>One Is Greater Than Zero</strong>.</p>
+		<div class="video-wrapper">
+			<iframe width="100%" height="480" src="https://www.youtube.com/embed/4fsYWXrGGcE?rel=0" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<p>I will strongly recommend you to subscribe to Gary Vaynerchuk's youtube channel and get the <strong>absolute raw</strong> knowledge. No bullshit.</p>
+		<script src="https://apis.google.com/js/platform.js"></script>
+		<div class="g-ytsubscribe" data-channel="GaryVaynerchuk" data-layout="full" data-count="default"></div>
+		<p><em class="text-sm">I am not paid to endorse him. I do it because I want to give him back whatever I can - even if it's just 1 new follower.</em></p>
+	</article>
+
+	<article class="text-edition">
+		<p>With every startup, the one problem that arises is "getting those first 10 people who would pay you". And how do you get those people? Looks like getting first 1000 customers is more difficult than getting the next 10,000.</p>
+	</article>
+
+	<article class="text-edition wisdom wisdom-blue" id="focus-on-little-things">
+		<span class="wisdom-label"><a href="#focus-on-little-things"><i class="fa fa-anchor"></i></a> Collection</span>
+		<h4 class="heading-edition">Focus On Little Things</h4>
+		<p><a href="pallavn">Pallav Nadhani</a> is founder & CEO of FusionCharts. He considers himself an <em>accidental entrepreneur</em>. In a podcast with <a href="http://twitter.com/veermishra0803">Veer Mishra</a>, he tells how he hated the ugly Excel reports and wrote a quick code to build beautiful charts for web.</p>
+		<blockquote>
+			<p>"The businesses are moving from carpet-bombing approach to sniper approach."</p>
+			<small>Pallav Nadhani</small>
+		</blockquote>
+		<p>The whole interview is worth listening to where Pallav talks about everything from building team, content, human-to-human sales and angel investments.</p>
+		<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/178701396&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+		<hr />
+		<p>Not only in getting customers, even when hiring, Pallav and his team put special focus on <strong>the little things</strong>.</p>
+		<blockquote class="twitter-tweet tw-align-center" lang="en"><p>One of core hiring mantras <a href="https://twitter.com/FusionCharts">@FusionCharts</a> : If you can’t do the little things right, you will never do the big things right.</p>&mdash; Pallav Nadhani (@pallavn) <a href="https://twitter.com/pallavn/status/487498090828410880">July 11, 2014</a></blockquote>
+	</article>
+
+	<article class="text-edition">
+		<p>In one of the <a href="http://paulgraham.com/ds.html">essays</a>, Paul Graham tells how Stripe got their first customers. They did something that became popular as "Collison Installation" at YC.</p>
+
+		<blockquote class="text-sm">
+			<p>More diffident founders ask "Will you try our beta?" and if the answer is yes, they say "Great, we'll send you a link." But the Collison brothers weren't going to wait. When anyone agreed to try Stripe they'd say "Right then, give me your laptop" and set them up on the spot.</p>
+		</blockquote>
+
+		<p>This approach doesn't scale when you recruit your 1000th user, but all scalable tricks don't work when you recruit your 2nd user. When small, <strong>do things that do not scale</strong>, because you can.</p>
+
+		<p>Like most small things, they aren't much individually, but when you do a lot of small things, they make a big impact. This is how exactly <a href="https://twitter.com/alexmturnbull">Alex</a> grew to 2,000+ customers.</p>
+	</article>
+
+	<article class="text-edition wisdom wisdom-green" id="unscalable-things">
+		<span class="wisdom-label"><a href="#unscalable-things"><i class="fa fa-anchor"></i></a> Blog Post</span>
+		<h4 class="heading-edition">How We Got 2,000+ Customers by Doing Things That Didn’t Scale</h4>
+		<p>Alex reached out to a lot of founders and asked the wrong questions: "How are you acquiring users?" He'd get some answers and ideas (referrals, upselling, advertising) but he was overwhelmed because these would help later-stage companies, not someone who's just getting started.</p>
+
+		<blockquote>
+			<p>"Look, I could tell you what we’re doing, but it wouldn’t help you. We have 10,000 customers. You have zero. You need to focus on your first five customers."</p>
+		</blockquote>
+
+		<p>Alex was told that those later-stage companies had fought hard for every single customer in their early days. They'd spend hours with each of their customers to give them positive experience. And now Alex does all these unscalable things that helped him grow Groove so quickly.</p>
+
+		<ul>
+			<li><strong>Welcome Email</strong>: The super-personalised email that asks why they signed up for Groove (demands an action) is what got engagement started. Sending email is easy, but then reading the replies and acting on each of those is unscalable.</li>
+			<li><strong>Periodic Chats</strong>: He carries out 10-15 min calls with each of his user to understand them better and explain their roadmap. It is tough, but leaves an awesome impression.</li>
+			<li><strong>Engagement</strong>: Alex's blog is shit-popular and still he replies to every comment that he gets (~150 on every post).</li>
+			<li><strong>Scrapping</strong>: When a blogger compared Groove with other softwares and chose the other app as winner, he got in touch with him, asked the reason, fixed the problem and "earned him back". Scrapping every "one that got away" is not scalable, but worth it.</li>
+		</ul>
+
+		<a href="https://www.groovehq.com/blog/non-scaleable-growth-tactics" class="text-sm">Read Complete Post <i class="fa fa-angle-double-right"></i></a>
+	</article>
+
+	<hr />
+
+	<article class="text-edition">
+		<h3 class="heading-edition">In closing</h3>
+		<p>Oh, and it's Valentine's Day. Be it the matters of business or hearts, <strong>little things has big impact</strong>. The time has changed - everyone has <a href="/editions/passenger-or-pilot">choices</a>. To make them choose to listen to you, give them a better reason than, "Because I am speaking".</p>
+
+		<p>When little, do the little things - because they touch <span class="text-danger">♥</span>s.</p>
+
+		<p class="text-center"><a href="https://twitter.com/intent/tweet?button_hashtag=TheLittleThings&text=It's%20not%20the%20big%20banner%20that%20gets%20the%20attention.%20They're%20the%20little%20things%20that%20touch%20hearts." class="twitter-hashtag-button" data-size="large" data-related="mohitmamoria,horntell" data-url="http://elevator.horntell.com/editions/the-little-things">Tweet #TheLittleThings</a></p>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 	</article>
 @endsection
