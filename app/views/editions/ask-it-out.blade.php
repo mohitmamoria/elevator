cee829ffa9887fe2ee2237cfe39655665df7d2a4
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-17 14:26:24 +0530

    first draft done

--- app/views/editions/ask-it-out.blade.php
+++ app/views/editions/ask-it-out.blade.php
@@ -4,16 +4,108 @@
 
 @section('edition')
 	<article class="text-edition">
-		<p>Often I tend to come out as shameless. Or more aptly - creepy, because I ask the tough questions right on face rather than trying to assume answers myself and base whole decision on nothing more than an assumption.</p>
+		<p>Often I tend to come out as shameless. Or more aptly - creepy, because I ask the tough questions right on face rather than trying to assume answers myself and base whole decision on them.</p>
 
-		<p>I recall, when launching Horntell at an event, I came to know Rajan Anandan (of Google) is among the audience and was looking to make one of his angel investment there itself. What did I do? From the stage, I asked him for his money:</p>
+		<p>I recall, when launching <a href="http://horntell.com">Horntell</a> at an event, I came to know <a href="https://twitter.com/RajanAnandan">Rajan Anandan</a> (of Google) was in audience looking to make one of his angel investment there itself. What did I do? From the stage, I asked him for his money:</p>
 
 		<blockquote>
 			<p>"I've been told that you don't get what you don't ask for. So, let me just do it right here. Mr Rajan Anandan, may we have your money?"</p>
 		</blockquote>
 
-		<p>My question was answered by whole audience with a loud round of applause. Rajan gave us some love on Twitter and connected us with some helpful people (he couldn't put his own money because of one of his competing investment).</p>
+		<p>My question was answered by whole audience with a loud round of applause. Rajan gave us <a href="https://twitter.com/RajanAnandan/status/541137770744258560">some love on Twitter</a> and connected us with some helpful people (though he couldn't put his own money because of one of his competing investment).</p>
 
 		<p>You don't get answers until you ask out the questions.</p>
+	</article>
+
+	<article class="text-edition">
+		<blockquote class="block">
+			<p>The wise man doesn't give the right answers, he poses the right questions.</p>
+			<small>Claude Levi-Strauss</small>
+		</blockquote>
+
+		<p>But, how do you ask the right questions? That's one right question to be asked.</p>
+	</article>
+
+	<article class="text-edition wisdom wisdom-green" id="how-to-ask-good-questions">
+		<span class="wisdom-label"><a href="#how-to-ask-good-questions"><i class="fa fa-anchor"></i></a> Article</span>
+		<h4 class="heading-edition">How to Be Amazingly Good at Asking Questions</h4>
+		<p>When asking a question, you should be very clear about what you need for an answer. Do you need a factually correct answer? Do you need an expert opinion? Do you need a well-reasoned judgment? Be precise.</p>
+
+		<ul>
+			<li><strong>Don't ask Yes/No questions</strong>: Instead ask the questions with an open-end. Questions with 'who,' 'what,' 'where,' 'when,' 'how,' or 'why' lead to people giving some thought to their answers.</li>
+			<li><strong>Dig deeper</strong>: Always consider using follow-up questions like, 'What makes you think that?'</li>
+			<li><strong>Stay silent</strong>: People have more information and will bring it out in moments of silence to fill in the gaps.</li>
+			<li><strong>Don't interupt</strong>: Ask your question, then let the person answer it in full, even when you think you are not getting the answer you want.</li>
+		</ul>
+
+		<a href="http://www.lifehack.org/articles/communication/how-amazingly-good-asking-questions.html" class="text-sm">Read Complete Post <i class="fa fa-angle-double-right"></i></a>
+	</article>
+
+	<article class="text-edition">
+		<p>No question is stupid. If it doesn't quench your curiosity in the way you want, it will be helpful in some other way. This is exactly what happens with Calvin.</p>
+		<img src="/assets/images/ask-it-out/hobbes-1.gif" alt="Calvin and Hobbes" class="img-center img-responsive img-thumbnail">
+	</article>
+
+	<article class="text-edition">
+		
+	</article>
+
+	<article class="text-edition wisdom wisdom-red" id="gary-corner">
+		<span class="wisdom-label"><a href="#gary-corner"><i class="fa fa-anchor"></i></a> Gary's Corner</span>
+		<h4 class="heading-edition">#AskGaryVee: Episode 40</h4>
+		<p>How can we forget our own <a href="https://twitter.com/garyvee">Gary</a> when it comes down to 'asking' questions. He does a whole Youtube show (<a href="https://www.youtube.com/playlist?list=PLfA33-E9P7FC0AoARnMLvgFgESJe4_Ngs">#AskGaryVee</a>) where anyone can ask him questions and he'll answer.</p>
+
+		<dl>
+			<dt>How do you prioritize which project to execute first? (at 00:33)</dt>
+			<dd>I prioritize base on two things equally - my intuition (fixing the future) and on what thigns are on fire (fixing the present).</dd>
+			<hr />
+			<dt>If you were to jump into a time machine, travel back to speak to your 21 year old self, and only have 1 minute. What do you say? (at 02:59)</dt>
+			<dd>I'd say, "Have a little more fun in life. Don't stress over work too much. You're gonna accomplish things. You need to spend a little more time with your friends, you can go on one more vacation."</dd>
+		</dl>
+
+		<div class="video-wrapper">
+			<iframe width="100%" height="480" src="https://www.youtube.com/embed/VY_dDBtPuJo?rel=0" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</article>
+
+	<article class="text-edition">
+		<p>In <a href="/editions/passenger-or-pilot#no-to-customer">one edition</a>, we saw how saying 'No' is important and why we should say it more often. But, human nature has it that the reason we are afraid of asking questions is because of this two-letter word. Let's see what <a href="https://twitter.com/iwearyourshirt">Jason Zook</a> says.</p>
+	</article>
+
+	<article class="text-edition wisdom wisdom-blue" id="no-ask-no-getting">
+		<span class="wisdom-label"><a href="#no-ask-no-getting"><i class="fa fa-anchor"></i></a> Article</span>
+		<h4 class="heading-edition">You Don't Get What You Don't Ask For</h4>
+		<p>We, humans are afraid to ask things - including <em>'asking people to buy our product'</em>, <em>'asking that girl out'</em>, <em>'asking for a raise'</em>.</p>
+		<blockquote>
+			<p>We're afraid to ask because we fear rejection.</p>
+		</blockquote>
+
+		<p>Rejection is unbelievably powerful to derail people from asking for that thing again. It makes us feel we don't deserve what we are asking for. The feeling of getting rejected after you've asked someone out for a date sticks with you for years to come.</p>
+		<blockquote>
+			<p>100 people could tell you how freaking amazing you look today, but if one person says you look like crap, those 100 positive messages won't matter.</p>
+		</blockquote>
+
+		<p>If Jason has to credit his success to something, it'd be these two things:</p>
+		<ul>
+			<li>I wasn't afraid to ask for things most people wouldn't dare ask for.</li>
+			<li>I was willing to work my ass off to get the thing I wanted, because it was something I was really passionate about.</li>
+		</ul>
+
+		<p>People may go awe when they hear that he made $1,000,000 but what they don't get to hear is how much 'ask' Jason hsa done to reach there. They don't get to know about those 15000 emails that he sent to reach there. He overcame his fear of 'making the ask' was that he believed in himself.</p>
+
+		<a href="http://www.inc.com/jason-surfrapp/you-don-t-get-what-you-don-t-ask-for.html" class="text-sm">Read Complete Post <i class="fa fa-angle-double-right"></i></a>
+	</article>
+
+	<article class="text-edition wisdom wisdom-orange" id="asking-questions">
+		<span class="wisdom-label"><a href="#asking-questions"><i class="fa fa-anchor"></i></a>Randoms</span>
+		<h4 class="heading-edition">Questions > Answers</h4>
+
+		<img src="/assets/images/ask-it-out/intelligence.jpg" alt="Intelligence" class="img-center img-responsive img-thumbnail">
+		<hr />
+		<blockquote class="twitter-tweet tw-align-center" lang="en"><p>The most powerful way to win an argument is by asking questions. You&#39;d be surprised at how it can make people see the flaws in their logic.</p>&mdash; Fact (@Fact) <a href="https://twitter.com/Fact/status/567362922699968512">February 16, 2015</a></blockquote>
+	</article>
+
+	<article class="text-edition">
+		<p></p>
 	</article>
 @endsection
