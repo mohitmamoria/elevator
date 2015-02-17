365748f292ac78dc136168636cfd9bbfde3fb8db
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-17 12:00:09 +0530

    starting a new edition

---
+++ app/views/editions/ask-it-out.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.edition')
+
+{{-- Ask It Out --}}
+
+@section('edition')
+	<article class="text-edition">
+		<p>Often I tend to come out as shameless. Or more aptly - creepy, because I ask the tough questions right on face rather than trying to assume answers myself and base whole decision on nothing more than an assumption.</p>
+
+		<p>I recall, when launching Horntell at an event, I came to know Rajan Anandan (of Google) is among the audience and was looking to make one of his angel investment there itself. What did I do? From the stage, I asked him for his money:</p>
+
+		<blockquote>
+			<p>"I've been told that you don't get what you don't ask for. So, let me just do it right here. Mr Rajan Anandan, may we have your money?"</p>
+		</blockquote>
+
+		<p>My question was answered by whole audience with a loud round of applause. Rajan gave us some love on Twitter and connected us with some helpful people (he couldn't put his own money because of one of his competing investment).</p>
+
+		<p>You don't get answers until you ask out the questions.</p>
+	</article>
+@endsection
