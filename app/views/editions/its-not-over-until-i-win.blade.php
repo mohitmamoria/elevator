b540075897563dee4060a29ae83ae0621dbebf6e
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-25 18:33:14 +0530

    added the text content

--- app/views/editions/its-not-over-until-i-win.blade.php
+++ app/views/editions/its-not-over-until-i-win.blade.php
@@ -4,6 +4,61 @@
 
 @section('edition')
 	<article class="text-edition">
-		<p>I was weak this week. Not physically, but mentally.</p>
+		<p class="text-sm"><em>Note: This will be a different kind of edition. By the end of this edition, you'll know why I couldn't collect the knowledge nuggets this week.</em></p>
+		<p>I am a dick. I wasn't one but now, I am, because of how I acted for last couple of days. Let me tell you the story.</p>
+		
+		<h3 class="heading-edition">The Blow</h3>
+		<p>We were doing trial run for first batch of <a href="http://horntell.com">Horntell</a> early birds, and after their trial run, we got emails from them telling us none of them will buy Horntell.</p>
+		<p><em>Whaaaaaaaat?!?! None of them? WTF!</em></p>
+		<p>During the trial period, they kept giving good feedback and potential future features, but as soon as the trial ended, they stopped using it. I went crazy. Although bootstrapped, we had put the expensive infrastructure, made a good product, did things that didn't scale (flying all over the country to meet and understand requirements of each prospect) and we got these emails in one single day telling us that <strong>they won't buy</strong>.</p>
+
+		<h3 class="heading-edition">Here I Become Douchebag</h3>
+		<p>I started doubting our product. I started doubting my decisions (of course, some were wrong but some were right too). I started doubting our existence as a startup. The ideas to shut it down started growing on me. I started calculating how much I had spent on this in the last couple of years and how much more runway was left. My co-founder had left (still friends though). Year-end maintenance cost of US entity burnt a huge hole in my pocket. I started wondering if I should recommend my whole team to some good place and think about my future from scratch again. (I now know how Elon Musk had been feeling when Tesla wasn't picking up.)</p>
+		<p>I am telling you, I was being a dick. And I wasn't even doing a good job at it. I was kinda being a weird kind of dick. Probably because I started feeling the pressure from all around. I was doing a really bad job at handling pressure.</p>
+	</article>
+
+	<hr />
+
+	<article class="text-edition">
+		<h3 class="heading-edition">Here I Return</h3>
+		<p>But hey, that was whole 2 days ago. Now, I am back. That too, with a BANG!</p>
+		<p><em>Did I get cash in bank from somewhere? Did I get some idea to fix the things? What made me return back?</em> This...</p>
+		<p><strong>Talking to people.</strong></p>
+
+		<p>I called up the users, understood the reason why they couldn't continue with Horntell. I talked to them not like a salesman but like someone who has nothing to lose but everything to gain. I talked to them like I was learning something from the discussion. Talks went successful and I understood the reason. The reason they couldn't buy was two-fold.</p>
+
+		<ul>
+			<li>We had too many features and this was confusing them (and also increased the overall cost of product).</li>
+			<li>The integration was too tight for their taste, which required observable amount of efforts and time.</li>
+		</ul>
+
+		<p>Not just users, I talked a lot to my team too to discuss things from their perspectives. I spent nights (they were sleepless anyway) wondering (or shedding a couple of tears) and I think, we've reached to some solutions.</p>
+
+		<p><strong>We will be shutting down all of our features except just one. It will bring down the running cost, thus costing less to our users while also making the integration of no more than 10 minutes.</strong></p>
+	</article>
+
+	<article class="text-edition">
+		<h3 class="heading-edition">It's Not Over</h3>
+		<p>And these lines from the video down below was another reason that I am back.</p>
+
+		<p><em>Everyday, we face rejection of a NO, we have a meeting and no one shows up or somebody says, "you can count on me," but they don't come through. What if we have that kind of attitude because we possess, no body believes in you, you've lost again and again and again, your life's cut-off but you're still looking at your dream. You're viewing it everyday and saying it to yourself...</em></p>
+		<p class="text-center"><strong>"It's Not Over, Until I Win".</strong></p>
+		<div class="video-wrapper">
+			<iframe width="100%" height="480" src="https://www.youtube.com/embed/g-jwWYX7Jlo?rel=0" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<hr/>
+		<p>I am not giving up until I believe in the work I am doing. Hardships come. Brick walls come.</p>
+		<blockquote>
+			<p>The brick walls are there for a reason. The brick walls are not there to keep us out. The brick walls are there to give us a chance to show how badly we want something. Because the brick walls are there to stop the people who don’t want it badly enough. They’re there to stop the other people.</p>
+			<small>Randy Pausch</small>
+		</blockquote>
+	</article>
+
+	<hr />
+
+	<article class="text-edition">
+		<h3 class="text-center">I am still hungry. I will stay hungry.</h3>
+
+		<p class="text-center"><a href="https://twitter.com/intent/tweet?button_hashtag=UntilIWin&text=I%20am%20still%20hungry.%20I%20will%20stay%20hungry.%20It's%20not%20over,%20until%20I%20win." class="twitter-hashtag-button" data-size="large" data-related="mohitmamoria,horntell" data-url="http://elevator.horntell.com/editions/its-not-over-until-i-win">Tweet #UntilIWin</a></p>
 	</article>
 @endsection
