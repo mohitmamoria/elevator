3dbe01868d40d1b2484c300610d85aae1ac21156
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-25 14:59:00 +0530

    created new blank edition

---
+++ app/views/editions/its-not-over-until-i-win.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.edition')
+
+{{-- It's Not Over, Until I Win --}}
+
+@section('edition')
+	<article class="text-edition">
+		<p>I was weak this week. Not physically, but mentally.</p>
+	</article>
+@endsection
