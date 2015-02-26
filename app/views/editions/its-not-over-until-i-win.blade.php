e517399f6c4bc9daf086bf0aae5470cc79f04a22
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-26 11:56:24 +0530

    made some final touches

--- app/views/editions/its-not-over-until-i-win.blade.php
+++ app/views/editions/its-not-over-until-i-win.blade.php
@@ -25,21 +25,22 @@
 		<p><em>Did I get cash in bank from somewhere? Did I get some idea to fix the things? What made me return back?</em> This...</p>
 		<p><strong>Talking to people.</strong></p>
 
-		<p>I called up the users, understood the reason why they couldn't continue with Horntell. I talked to them not like a salesman but like someone who has nothing to lose but everything to gain. I talked to them like I was learning something from the discussion. Talks went successful and I understood the reason. The reason they couldn't buy was two-fold.</p>
+		<p>I called up the users, understood the reason why they couldn't continue with Horntell. I talked to them not like a salesman but like someone who had nothing to lose but everything to gain. I talked to them like I was learning something from the discussion. Talks went successful and I understood the reason. The reason they couldn't buy was two-fold.</p>
 
 		<ul>
 			<li>We had too many features and this was confusing them (and also increased the overall cost of product).</li>
 			<li>The integration was too tight for their taste, which required observable amount of efforts and time.</li>
 		</ul>
 
-		<p>Not just users, I talked a lot to my team too to discuss things from their perspectives. I spent nights (they were sleepless anyway) wondering (or shedding a couple of tears) and I think, we've reached to some solutions.</p>
+		<p>Not just users, I talked a lot to my team too to discuss things from their perspectives. I spent nights (they were sleepless anyway) wondering (or shedding tears) and I think, we've reached to some conclusion.</p>
 
-		<p><strong>We will be shutting down all of our features except just one. It will bring down the running cost, thus costing less to our users while also making the integration of no more than 10 minutes.</strong></p>
+		<p><strong>We will be shutting down all of our features except just one. It will bring down the running cost, thus costing less to our users while also making the integration of no longer than 10 minutes.</strong></p>
 
 		<img src="/assets/images/its-not-over-until-i-win/booyah.gif" alt="Booyah" class="img-center img-responsive img-thumbnail">
 	</article>
 
-	<article class="text-edition">
+	<article class="text-edition wisdom wisdom-green" id="its-not-over">
+		<span class="wisdom-label"><a href="#its-not-over"><i class="fa fa-anchor"></i></a>Video</span>
 		<h3 class="heading-edition">It's Not Over</h3>
 		<p>And these lines from the video down below was another reason that I am back.</p>
 
@@ -49,6 +50,9 @@
 			<iframe width="100%" height="480" src="https://www.youtube.com/embed/g-jwWYX7Jlo?rel=0" frameborder="0" allowfullscreen></iframe>
 		</div>
 		<hr/>
+	</article>
+
+	<article class="text-edition">
 		<p>I am not giving up until I believe in the work I am doing. Hardships come. Brick walls come.</p>
 		<blockquote>
 			<p>The brick walls are there for a reason. The brick walls are not there to keep us out. The brick walls are there to give us a chance to show how badly we want something. Because the brick walls are there to stop the people who don’t want it badly enough. They’re there to stop the other people.</p>
@@ -60,7 +64,7 @@
 		<span class="wisdom-label"><a href="#the-rabbit"><i class="fa fa-anchor"></i></a>Book</span>
 		<h4 class="heading-edition">Don't change the rabbit</h4>
 		
-		<p>Usually, I click photos of interesting parts of a book. I was browsing through those shots when I found this. I am glad that I have this habit of collecting such nuggets. The 4 sentences in the image below were fitting so well in the situation I was in.</p>
+		<p>Usually, I click photos of interesting parts of a book. I was browsing through those shots and I found this. I am glad that I have this habit of collecting such nuggets. The 4 sentences in the image below were fitting so well in the situation I was in. And these 4 sentences that I collected an year ago brought my confidence in myself back.</p>
 		<img src="/assets/images/its-not-over-until-i-win/rabbit.jpg" alt="Don't change the rabbit" class="img-center img-responsive img-thumbnail">
 		<p class="text-center text-xs">From the book 'The Habit of Winning' by Prakash Iyer</p>
 
@@ -72,6 +76,8 @@
 	<article class="text-edition">
 		<h3 class="text-center">I am still hungry. I will stay hungry.</h3>
 
-		<p class="text-center"><a href="https://twitter.com/intent/tweet?button_hashtag=IAmHungry&text=I%20am%20still%20hungry.%20I%20will%20stay%20hungry.%20It's%20not%20over,%20until%20I%20win." class="twitter-hashtag-button" data-size="large" data-related="mohitmamoria,horntell" data-url="http://elevator.horntell.com/editions/its-not-over-until-i-win">Tweet #IAmHungry</a></p>
+		<p class="text-center">Are you hungry too? I'd love to talk to you.</p>
+
+		<p class="text-center"><a href="https://twitter.com/intent/tweet?button_hashtag=IAmHungry&via=mohitmamoria&text=I%20am%20still%20hungry.%20I%20will%20stay%20hungry.%20It's%20not%20over,%20until%20I%20win." class="twitter-hashtag-button" data-size="large" data-related="mohitmamoria,horntell" data-url="http://elevator.horntell.com/editions/its-not-over-until-i-win">Tweet #IAmHungry</a></p>
 	</article>
 @endsection
